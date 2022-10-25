f62fcaa1778f99fc88b36a7348590e7dc614cb12
CONTRERAS LORENZO VICTOR CARLOS <victor.contreras@ine.mx>
2022-10-25 12:30:18 -0500

    Rutas anidadas listas

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Suspense } from 'react';
 import {
@@ -10,6 +11,7 @@ import {
 import { routes } from './routes';
 
 import logo from '../logo.svg'
+
 
 
 export const Navigation = () => {
